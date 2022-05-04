19cc3c31f586715413ae7fa09e5de87dfec19099
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2022-05-04 17:11:08 -0300

    feat: comparing the guess to the secret number

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 pub fn main() {
     println!("Guess the number!");
-    println!("Please input the number:");
 
-    // create a mutable variable of type string and set :: (associated function that's implemented on a type)
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {}", secret_number);
+
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
 
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
